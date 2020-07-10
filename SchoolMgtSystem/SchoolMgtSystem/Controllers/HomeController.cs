d0e5937d67a69c8921811148a502aed7cb62d76f
Trendon Cato <electric5388@hotmail.com>
2020-07-11 00:32:18 +0800

    Created User Management

---
+++ SchoolMgtSystem/SchoolMgtSystem/Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SchoolDBAccess;
+
+namespace SchoolMgtSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginUser(string email,string password)
+        {
+            try
+            {
+                if (email != null && password != null)
+                {
+                    var finduser = db.UserTables.Where(u => u.EmailAddress == email && u.Password == password).ToList();
+                    if(finduser.Count() == 1)
+                    {
+                        Session["UserID"] = finduser[0].UserID;
+                        Session["UserTypeID"] = finduser[0].UserTypeID;
+                        Session["FullName"] = finduser[0].FullName;
+                        Session["UserName"] = finduser[0].UserName;
+                        Session["Password"] = finduser[0].Password;
+                        Session["ContactNo"] = finduser[0].ContactNo;
+                        Session["EmailAddress"] = finduser[0].EmailAddress;
+                        Session["Address"] = finduser[0].Address;
+
+                        //UserTypeID UserTypeName
+                        //1   Admin
+                        //2   Operator
+                        //3   Teacher
+                        //4   Student
+
+                        string url = string.Empty;
+                        if (finduser[0].UserTypeID == 2)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 3)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 4)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 1)
+                        {
+                            url = "About";
+                        }
+                        else
+                        {
+                            url = "About";
+                        }
+
+                        return RedirectToAction(url);
+                    }
+                    else
+                    {
+                        Session["UserID"] = string.Empty;
+                        Session["UserTypeID"] = string.Empty;
+                        Session["FullName"] = string.Empty;
+                        Session["UserName"] = string.Empty;
+                        Session["Password"] = string.Empty;
+                        Session["ContactNo"] = string.Empty;
+                        Session["EmailAddress"] = string.Empty;
+                        Session["Address"] = string.Empty;
+                        ViewBag.message = "User Name and Password is incorrect!";
+                    }
+                }
+                else
+                {
+                    Session["UserID"] = string.Empty;
+                    Session["UserTypeID"] = string.Empty;
+                    Session["FullName"] = string.Empty;
+                    Session["UserName"] = string.Empty;
+                    Session["Password"] = string.Empty;
+                    Session["ContactNo"] = string.Empty;
+                    Session["EmailAddress"] = string.Empty;
+                    Session["Address"] = string.Empty;
+                    ViewBag.message = "Some unexpected issue has occurred. Please try again!";
+                }
+            }
+            catch(Exception ex)
+            {
+                Session["UserID"] = string.Empty;
+                Session["UserTypeID"] = string.Empty;
+                Session["FullName"] = string.Empty;
+                Session["UserName"] = string.Empty;
+                Session["Password"] = string.Empty;
+                Session["ContactNo"] = string.Empty;
+                Session["EmailAddress"] = string.Empty;
+                Session["Address"] = string.Empty;
+                ViewBag.message = "Some unexpected issue has occurred. Please try again!";
+            }
+            return View("Login");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Welcome to Trent Bas School Management System.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["UserID"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            Session["FullName"] = string.Empty;
+            Session["UserName"] = string.Empty;
+            Session["Password"] = string.Empty;
+            Session["ContactNo"] = string.Empty;
+            Session["EmailAddress"] = string.Empty;
+            Session["Address"] = string.Empty;
+            return RedirectToAction("Login");
+        }
+    }
+}
